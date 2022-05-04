9e9437cc8e6c1916fdb9f81546e83c9e7506f623
unknown <cogdotnet03@iiht.tech>
2022-05-04 12:58:41 +0530

    New push added

---
+++ AirlineDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class AirlineDetails
+{
+	public AirlineDetails()
+	{
+    public string FlightName { get; set; }
+    public string FromPlace { get; set; }
+    public string ToPlace { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int Blocked { get; set; }
+    public string ScheduledDay { get; set; }
+    public int FlightNumber { get; set; }
+    public string InstrumentUsed { get; set; }
+    public int BusinessClassSeats { get; set; }
+    public int NonBusinessClassSeats { get; set; }
+    public int TicketCost { get; set; }
+    public int NoOfRows { get; set; }
+    public string Meal { get; set; }
+
+}
+}
---
+++ UserDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class UserDetails
+{
+	public UserDetails()
+	{
+    public string UserName { get; set; }
+    public string EmailId { get; set; }
+    public int age { get; set; }
+    public string Gender { get; set; }
+    public string Meal { get; set; }
+    public int SeatNo { get; set; }
+    public DateTime? CreatedOn { get; set; }
+    public DateTime? UpdatedOn { get; set; }
+    public DateTime? UpdatedOn { get; set; }
+}
+}
